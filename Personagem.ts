7b954f2639b31924dd79afb0ee6dd946494cd945
Rafael Pavanelli <rafaelzica86@hotmail.com>
2024-01-14 01:36:10 -0300

    descontinuado

---
+++ Personagem.ts
@@ -0,0 +1,15 @@
+//Nome,Energia,vida,Ataque e Defesa
+
+class Personagem{
+    nome:string = '';
+    energia:number = 0;
+    vida:number = 0;
+    ataque:number = 0;
+    defesa:number = 0;
+}
+
+let sansa: Personagem;
+
+sansa = new Personagem()
+console.log("Person =>", sansa);
+
